5c2cb5b20443193932f62330d3e89f8b22903059
Ayush15570 <ayushnjr1557@gmail.com>
2025-11-30 13:39:30 +0530

    Updated CORS, separated app.js and server.js for deployment

--- src/app.js
+++ src/app.js
@@ -5,46 +5,44 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true
+    origin: process.env.FRONTEND_URL,
+    credentials: true
 }))
+
 app.use(cookieParser())
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 
-
+// ROUTES...
 import userRouter from "./routes/user.routes.js"
-
-app.use("/api/v1/users",userRouter)
+app.use("/api/v1/users", userRouter)
 
 import practiceRouter from "./routes/practice.router.js"
-
-app.use("/api/v1/practice",practiceRouter)
-
+app.use("/api/v1/practice", practiceRouter)
 
 import admin from "./routes/admin.router.js"
+app.use("/api/v1/admin", admin)
 
-app.use("/api/v1/admin",admin)
-
-import leaderboardRouter from "./routes/leaderboard.router.js";
-app.use("/api/v1/leaderboard", leaderboardRouter);
+import leaderboardRouter from "./routes/leaderboard.router.js"
+app.use("/api/v1/leaderboard", leaderboardRouter)
 
 import recommendation from "./routes/crop_fertilizer.router.js"
-app.use("/api/v1/recommendation",recommendation)
+app.use("/api/v1/recommendation", recommendation)
 
 import crop from "./routes/cropIssue.router.js"
-app.use("/api/v1/crop",crop)
+app.use("/api/v1/crop", crop)
 
 import challenge from "./routes/dailyChallenge.route.js"
-app.use("/api/v1/challenge",challenge)
+app.use("/api/v1/challenge", challenge)
 
 import subscription from "./routes/subscription.router.js"
-app.use("/api/v1/subscription",subscription)
+app.use("/api/v1/subscription", subscription)
 
 import digitalProducts from "./routes/digitalProduct.router.js"
-app.use("/api/v1/products",digitalProducts)
+app.use("/api/v1/products", digitalProducts)
 
 import aiIssue from "./routes/ai.routes.js"
-app.use("/api/v1/ai",aiIssue)
+app.use("/api/v1/ai", aiIssue)
+
 export default app
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
 import app from './app.js'
-dotenv.config({
-    path:'./.env'
-})
 
-connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 4000,()=>{
-        console.log(`server is running at port : ${process.env.PORT}`)
+dotenv.config()
+
+connectDB().then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`Server running on port ${process.env.PORT}`)
     })
 })
 .catch((err) => {
-    console.log("MONGODB CONNECTION FAILED",err)
+    console.log("MONGO CONNECTION FAILED", err)
 })
